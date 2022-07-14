e7801fcb2bba09729a4b37f1ace284a214b0b39a
Even Brenden <evenbrenden@gmail.com>
2022-07-14 13:23:52 +0200

    Reformat svpn-login.nix

--- svpn-login.nix
+++ svpn-login.nix
@@ -1,20 +1,19 @@
 { pkgs }:
 
 let
-  svpn =
-    if !(builtins.pathExists ./linux_f5vpn.x86_64.deb)
-    then throw "svpn-login needs linux_f5vpn.x86_64.deb"
-    else ./linux_f5vpn.x86_64.deb;
-in
-  pkgs.stdenv.mkDerivation {
-    name = "svpn-login";
-    nativeBuildInputs = [ pkgs.dpkg pkgs.autoPatchelfHook ];
-    buildInputs = with pkgs; [ (python39.withPackages (ps: [ps.requests])) ];
-    unpackPhase = ":";
-    installPhase = ''
-      dpkg -x ${./linux_f5vpn.x86_64.deb} $out
-      install -m755 -D $out/opt/f5/vpn/svpn $out/bin/svpn
-      rm -rf $out/opt
-      install -m755 -D ${./svpn-login.py} $out/bin/svpn-login
-    '';
-  }
+  svpn = if !(builtins.pathExists ./linux_f5vpn.x86_64.deb) then
+    throw "svpn-login needs linux_f5vpn.x86_64.deb"
+  else
+    ./linux_f5vpn.x86_64.deb;
+in pkgs.stdenv.mkDerivation {
+  name = "svpn-login";
+  nativeBuildInputs = [ pkgs.dpkg pkgs.autoPatchelfHook ];
+  buildInputs = with pkgs; [ (python39.withPackages (ps: [ ps.requests ])) ];
+  unpackPhase = ":";
+  installPhase = ''
+    dpkg -x ${./linux_f5vpn.x86_64.deb} $out
+    install -m755 -D $out/opt/f5/vpn/svpn $out/bin/svpn
+    rm -rf $out/opt
+    install -m755 -D ${./svpn-login.py} $out/bin/svpn-login
+  '';
+}
