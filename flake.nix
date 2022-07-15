1c4b7728f4b9e6bd3595b47ca7de4081817c180f
Even Brenden <evenbrenden@gmail.com>
2022-07-15 10:35:25 +0200

    Nix derivation: Pass path to svpn dpkg
    So that the overlay can be consumed by other flakes.

--- flake.nix
+++ flake.nix
@@ -1,4 +1,5 @@
-# E.g. "nix run path:$(pwd) -- [arguments to svpn-login]" (impure)
+# This an impure flake that needs to find linux_f5vpn.x86_64.deb at the repo root.
+# Make sure that it does and do e.g.: nix run path:$(pwd) -- [arguments to svpn-login]
 {
   description = "F5 SSL VPN Command-Line Client";
 
@@ -7,7 +8,14 @@
       system = "x86_64-linux";
       pkgs = nixpkgs.legacyPackages.${system};
     in {
-      packages.${system}.default = pkgs.callPackage ./svpn-login.nix { };
+      packages.${system}.default =
+        if !(builtins.pathExists ./linux_f5vpn.x86_64.deb) then
+          throw "svpn-login needs linux_f5vpn.x86_64.deb"
+        else
+          pkgs.callPackage ./svpn-login.nix {
+            pkgs = pkgs;
+            svpn-path = ./linux_f5vpn.x86_64.deb;
+          };
       overlay = final: prev: { svpn-login = ./svpn-login.nix; };
     };
 }
--- svpn-login.nix
+++ svpn-login.nix
@@ -1,23 +1,16 @@
-{ pkgs }:
+{ pkgs, svpn-path }:
 
-let
-  svpn = let path = ./linux_f5vpn.x86_64.deb;
-  in if !(builtins.pathExists path) then
-    throw "svpn-login needs ${path}"
-  else
-    path;
-  svpn-login = ./svpn-login.py;
-in pkgs.stdenv.mkDerivation {
+pkgs.stdenv.mkDerivation {
   name = "svpn-login";
   nativeBuildInputs = with pkgs; [ autoPatchelfHook dpkg makeWrapper ];
   buildInputs = with pkgs;
     [ (python39.withPackages (pythonPkgs: [ pythonPkgs.requests ])) ];
   unpackPhase = ":";
   installPhase = ''
-    dpkg -x ${svpn} $out
+    dpkg -x ${svpn-path} $out
     install -m755 -D $out/opt/f5/vpn/svpn $out/bin/svpn
     rm -rf $out/opt
-    install -m755 -D ${svpn-login} $out/bin/svpn-login
+    install -m755 -D ${./svpn-login.py} $out/bin/svpn-login
     wrapProgram $out/bin/svpn-login --prefix PATH : $out/bin
   '';
   meta.mainProgram = "svpn-login";
